5ce56eac9f76da456b4450065d35310026adc003
Laurence Freeman <laurence.freeman3@outlook.com>
2022-11-28 20:34:56 +0000

    +- Prevented sprite fade triggering multiple times in succession

--- rondje-test/Assets/Scripts/Select.cs
+++ rondje-test/Assets/Scripts/Select.cs
@@ -1,17 +1,23 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Select : MonoBehaviour
 {
+    private DateTime prevTriggered;
     private Renderer spriteRend;
+    private Boolean fadeReady;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRend = GetComponent<Renderer>();
-        // Set colour to be 0 when the game starts
+
+        // Set alpha to be 0 when the game starts
         spriteRend.material.color = new Color(1, 1, 1, 0);
+
+        // Make sure when the game initially starts we can trigger the fade.
+        prevTriggered = DateTime.Now.AddSeconds(-10);
     }
 
     // Update is called once per frame
@@ -22,15 +28,45 @@ public class Select : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (!CanBeTriggered()) 
+            return;
+        
         StartCoroutine(FadeTo(1.0f, 1.0f));
+        fadeReady = false;
     }
 
     private IEnumerator OnMouseExit()
     {
-        yield return new WaitForSeconds(1);
-        StartCoroutine(FadeTo(0.0f, 1.0f));
+        if (!fadeReady)
+        {
+            yield return new WaitForSeconds(7);
+            prevTriggered = DateTime.Now;
+            StartCoroutine(FadeTo(0.0f, 1.0f));
+
+            // Set fadeReady back to true once the sprite has faded out so we can retrigger.
+            fadeReady = true;
+        }
     }
 
+    /// <summary>
+    /// Returns true if last fade happened more than 10 seconds ago, false if not
+    /// </summary>
+    /// <returns>boolean</returns>
+    private bool CanBeTriggered()
+    {
+        TimeSpan timeDifference = DateTime.Now - prevTriggered;
+        if (timeDifference.Seconds > 10)
+            return true;
+
+        return false;
+    }
+
+    /// <summary>
+    /// Fades the alpha of the sprite to a given value over a given time period.
+    /// </summary>
+    /// <param name="aValue">Alpha value to fade to</param>
+    /// <param name="aTime">Time over which the fade should occur</param>
+    /// <returns></returns>
     IEnumerator FadeTo(float aValue, float aTime)
     {
         float alpha = spriteRend.material.color.a;
