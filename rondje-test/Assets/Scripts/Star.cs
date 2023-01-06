b49974024baa2ebb21e78fc8b78e374e0d72a5c6
Sanmoroo <sanmoroo@outlook.com>
2023-01-06 18:55:42 +0100

    ++ Ster Animatie & Idle

---
+++ rondje-test/Assets/Scripts/Star.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Star : InteractableSprite
+{
+    public override float TimeToFade => 1.0f;
+    public override string AttachedAnimation => "Star";
+    public override string AttachedSound => "Rechts_Onder";
+    public override string AttachedTrigger => "StarTrigger";
+    public override bool InteractionReady { get; set; }
+}
--- rondje-test/Assets/Scripts/TopLeft.cs
+++ rondje-test/Assets/Scripts/TopLeft.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class TopLeft : InteractableSprite
 {
-    public override float TimeToFade => 1.0f;
+    public override float TimeToFade => 0.1f;
     public override string AttachedAnimation => "test";
     public override string AttachedSound => "Rechts_Onder";
     public override string AttachedTrigger => "TopLeftTrigger";
