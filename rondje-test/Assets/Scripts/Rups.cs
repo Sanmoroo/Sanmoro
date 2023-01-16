41b642d6a3e1926ce6f3fb5df338cd087222808e
Sanmoroo <sanmoroo@outlook.com>
2023-01-16 05:29:04 +0100

    + RUPS is in

---
+++ rondje-test/Assets/Scripts/Rups.cs
@@ -0,0 +1,11 @@
+
+using System;
+
+public class Rups : InteractableSprite
+{
+    public override float TimeToFade => 0.1f;
+    public override string AttachedAnimation => "Rups_Incomming";
+    public override string AttachedSound => "Rups";
+    public override string AttachedTrigger => "Rups_Trigger";
+    public override bool UntriggeredInteraction { get; set; }
+}
