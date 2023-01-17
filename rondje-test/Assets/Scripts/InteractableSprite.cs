2d3d3229397d46b4ecb12273164bfc72847186d1
Sanmoroo <sanmoroo@outlook.com>
2023-01-17 22:31:51 +0100

    + For Lowwons :3

--- rondje-test/Assets/Scripts/InteractableSprite.cs
+++ rondje-test/Assets/Scripts/InteractableSprite.cs
@@ -39,7 +39,7 @@ public abstract class InteractableSprite : MonoBehaviour
         lastTriggered = DateTime.Now;
 
         // Change number in add seconds to increase the delay between the game starting and interactions being possible
-        introFinished = DateTime.Now.AddSeconds(37);
+        introFinished = DateTime.Now.AddSeconds(30.5);
 
         // Set alpha of sprites to be 0 when the game starts
         spriteRend.color = new Color(1, 1, 1, 0);
