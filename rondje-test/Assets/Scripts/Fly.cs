fe464ccc900f89339c32d1c1750d5175870489a5
Sanmoroo <sanmoroo@outlook.com>
2023-01-17 18:02:12 +0100

    +/- not sure, Jeremy checkup

--- rondje-test/Assets/Scripts/Fly.cs
+++ rondje-test/Assets/Scripts/Fly.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Fly : InteractableSprite
 {
-    public override float TimeToFade => 0.1f;
+    public override float TimeToFade => 1f;
     public override string AttachedAnimation => "Fly_Incomming";
     public override string AttachedSound => "Fly";
     public override string AttachedTrigger => "Fly_Trigger";
--- rondje-test/Assets/Scripts/InteractableSprite.cs
+++ rondje-test/Assets/Scripts/InteractableSprite.cs
@@ -37,7 +37,7 @@ public abstract class InteractableSprite : MonoBehaviour
         lastTriggered = DateTime.Now;
 
         // Change number in add seconds to increase the delay between the game starting and interactions being possible
-        introFinished = DateTime.Now.AddSeconds(32);
+        introFinished = DateTime.Now.AddSeconds(37);
 
         // Set alpha of sprites to be 0 when the game starts
         spriteRend.material.color = new Color(1, 1, 1, 0);
