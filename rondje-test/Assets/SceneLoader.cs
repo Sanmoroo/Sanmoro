ba903c7d245c60afa5c8025d1be4c949934a7329
Laurence Freeman <laurence.freeman3@outlook.com>
2023-01-18 19:49:24 +0000

    + Fixed previous commit (unsaved changes)

---
+++ rondje-test/Assets/SceneLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Set as our CrossFade animator in the inspector.
+    public Animator transition;
+
+    public IEnumerator LoadSceneAsync(string sceneName, LoadSceneMode loadSceneMode)
+    {
+        transition.SetTrigger("Start");
+
+        AsyncOperation loadingOperation = SceneManager.LoadSceneAsync(sceneName, loadSceneMode);
+
+        while (!loadingOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
